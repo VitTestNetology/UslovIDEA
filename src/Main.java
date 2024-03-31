e850c489bfa320bca9896bb5399aa7209b2bd724
VIT test <pysla.ru@gmail.com>
2024-04-01 01:49:43 +0300

    Change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int Price = 25689;
+        int price = 25689;
         System.out.println("Начисленно миль:");
 
-        int Mile;
-        if (Price >= 20) {
-            Mile = Price/20;
-            System.out.println(Mile);
+        int mile;
+        if (price >= 20) {
+            mile = price / 20;
+            System.out.println(mile);
         } else {
             System.out.println("0");
         }
@@ -14,11 +14,4 @@ public class Main {
 }
 
 
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
-
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
 
